f85e761fb9a1134596109ee8cef75005f6336ecb
Agatha A <142282616+Adjoa20@users.noreply.github.com>
2024-07-17 19:12:24 -0400

    added in assets
    Added in the images and icons that will be used for the menu

---
+++ frontend/src/assets/assets.js
@@ -0,0 +1,122 @@
+import basket_icon from './basket_icon.png'
+import logo from './logo.png'
+import header_img from './header_img.png'
+import search_icon from './search_icon.png'
+import menu_1 from './menu_1.png'
+import menu_2 from './menu_2.png'
+import menu_3 from './menu_3.png'
+
+import food_1 from './food_1.png'
+import food_2 from './food_2.png'
+import food_3 from './food_3.png'
+import food_4 from './food_4.png'
+import food_5 from './food_5.png'
+import food_6 from './food_6.png'
+import food_7 from './food_7.png'
+
+import add_icon_white from './add_icon_white.png'
+import add_icon_green from './add_icon_green.png'
+import remove_icon_red from './remove_icon_red.png'
+import app_store from './app_store.png'
+import play_store from './play_store.png'
+import linkedin_icon from './linkedin_icon.png'
+import facebook_icon from './facebook_icon.png'
+import twitter_icon from './twitter_icon.png'
+import cross_icon from './cross_icon.png'
+import selector_icon from './selector_icon.png'
+import rating_starts from './rating_starts.png'
+import profile_icon from './profile_icon.png'
+import bag_icon from './bag_icon.png'
+import logout_icon from './logout_icon.png'
+import parcel_icon from './parcel_icon.png'
+
+export const assets = {
+    logo,
+    basket_icon,
+    header_img,
+    search_icon,
+    rating_starts,
+    add_icon_green,
+    add_icon_white,
+    remove_icon_red,
+    app_store,
+    play_store,
+    linkedin_icon,
+    facebook_icon,
+    twitter_icon,
+    cross_icon,
+    selector_icon,
+    profile_icon,
+    logout_icon,
+    bag_icon,
+    parcel_icon
+}
+
+export const menu_list = [
+    {
+        menu_name: "Fufu",
+        menu_image: menu_1
+    },
+    {
+        menu_name: "Banku",
+        menu_image: menu_2
+    },
+    {
+        menu_name: "Rice",
+        menu_image: menu_3
+    },
+    ]
+
+export const food_list = [
+    {
+        _id: "1",
+        name: "Fufu with Light Soup",
+        image: food_3,
+        price: 12,
+        description: "",
+        category: "Fufu"
+    },
+    {
+        _id: "2",
+        name: "Fufu with Palmnut Soup",
+        image: food_4,
+        price: 18,
+        description: "",
+        category: "Fufu"
+    }, {
+        _id: "3",
+        name: "Fufu with Groundnut Soup",
+        image: food_5,
+        price: 16,
+        description: "",
+        category: "Fufu"
+    }, {
+        _id: "4",
+        name: "Banku with Hot Pepper & Tilapia",
+        image: food_1,
+        price: 24,
+        description: "",
+        category: "Banku"
+    }, {
+        _id: "5",
+        name: "Banku with Okro Soup",
+        image: food_2,
+        price: 14,
+        description: "",
+        category: "Banku"
+    }, {
+        _id: "6",
+        name: "Jollof Rice with Chicken",
+        image: food_7,
+        price: 12,
+        description: "",
+        category: "Rice"
+    }, {
+        _id: "7",
+        name: "Plain Rice with Stew",
+        image: food_6,
+        price: 20,
+        description: "",
+        category: "Rice"
+    }, 
+]
