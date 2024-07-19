dfcb49d5196bd90ef4a69b7bf544bc7bf035f68e
Agatha A <142282616+Adjoa20@users.noreply.github.com>
2024-07-18 21:35:45 -0400

    created the header

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -3,8 +3,12 @@ import './Header.css'
 
 const Header = () => {
   return (
-    <div>
-      
+    <div className='header'>
+      <div className="header-contents">
+        <h2>Order Here</h2>
+        <p>Gyamfua Food Court: Savor the Soul of Ghana, One Click at a Time!</p>
+        <button>View Menu</button>
+      </div>
     </div>
   )
 }
