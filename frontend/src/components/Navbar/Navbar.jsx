b5693ad8459d373593804d1da073fe54df2a2269
Agatha A <142282616+Adjoa20@users.noreply.github.com>
2024-07-18 21:00:50 -0400

    Created home, cart and placeorder folders
    also created the home.jsx file in the home folder

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -3,24 +3,28 @@ import "./Navbar.css"
 import { assets } from '../../assets/assets'
 
 const Navbar = () => {
-
+    // THIS IS TO CHANGE THE UNDERLINE ON CLICK FOR THE MENU 
     const [menu, setMenu] = useState('home')
 
   return (
     <div className='navbar'>
       <img src={assets.logo} alt='' className='logo'/>
+      {/* CREATED A LIST FOR THE NAVBAR MENU */}
       <ul className="navbar-menu">
-      <li className={menu==='home'?'active':''}>Home</li>
-      <li className={menu==='menu'?'active':''}>Menu</li>
-      <li className={menu==='mobile'?'active':''}>Mobile</li>
-      <li className={menu==='contact-us'?'active':''}>Contact us</li>
+        {/* CREATED A ACTIVE ON CLICK FOR WHEN USERS SWITCH TABS */}
+      <li onClick={()=>setMenu('home')} className={menu==='home'?'active':''}>Home</li>
+      <li onClick={()=>setMenu('menu')} className={menu==='menu'?'active':''}>Menu</li>
+      <li onClick={()=>setMenu('mobile')} className={menu==='mobile'?'active':''}>Mobile</li>
+      <li onClick={()=>setMenu('contact-us')} className={menu==='contact-us'?'active':''}>Contact us</li>
       </ul>
+      {/* PUT THE SEARCH AND BASKET ICONS IN A DIV TO HAVE IT SET TO THE RIGHT OF THE SCREEN */}
       <div className="navbar-right">
         <img src={assets.search_icon} alt="" />
         <div className="navbar-search-icon">
             <img src={assets.basket_icon} alt="" />
             <div className="dot"></div>
         </div>
+        {/* CREATED A BUTTON FOR SIGN IN */}
         <button>sign in</button>
       </div>
     </div>
